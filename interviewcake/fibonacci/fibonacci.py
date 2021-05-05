820b98ec10696f59aa865f6afe712ebc476e7873
David Ellinger <david.ellinger@me.com>
2021-05-04 22:21:49 -0400

    Added interviewcake fibonacci email question

---
+++ interviewcake/fibonacci/fibonacci.py
@@ -0,0 +1,14 @@
+"""
+Write a function fib() that takes an integer n
+and returns the nth Fibonacci number.
+"""
+
+
+def fib(n):
+
+    # Compute the nth Fibonacci number
+
+    number = None
+    for i in range(n):
+        number = i
+    return number
---
+++ interviewcake/fibonacci/test_fibonacci.py
@@ -0,0 +1,43 @@
+from .fibonacci import fib
+import pytest
+
+
+def test_zeroth_fibonacci():
+    actual = fib(0)
+    expected = 0
+    assert actual == expected
+
+
+def test_first_fibonacci():
+    actual = fib(1)
+    expected = 1
+    assert actual == expected
+
+
+def test_second_fibonacci():
+    actual = fib(2)
+    expected = 1
+    assert actual == expected
+
+
+def test_third_fibonacci():
+    actual = fib(3)
+    expected = 2
+    assert actual == expected
+
+
+def test_fifth_fibonacci():
+    actual = fib(5)
+    expected = 5
+    assert actual == expected
+
+
+def test_tenth_fibonacci():
+    actual = fib(10)
+    expected = 55
+    assert actual == expected
+
+
+def test_negative_fibonacci():
+    with pytest.raises(Exception):
+        fib(-1)

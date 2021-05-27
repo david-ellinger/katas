3219a40268b9be9143e6db9069eb28b42e25c6c3
David Ellinger <david.ellinger@me.com>
2021-05-26 22:16:21 -0400

    Fixed message encode tests

--- projects/message_encode_decode/tests/test_decode_encode.py
+++ projects/message_encode_decode/tests/test_decode_encode.py
@@ -1,15 +1,17 @@
-from app.app import encode, decode
+from app.app import Application
 
 MESSAGE = "This is a test message"
 KEY = "This is a test key"
 EXPECTED_ENCODING = "wqjDkMOSw6ZAw5LDpkDDgkDDqMOKw6bDqEDDmMOKw6zDh8OJw5DDmA=="
 
 
-def test_encode():
-    result = encode(KEY, MESSAGE)
+def test_encode(mocker):
+    app = Application()
+    result = app.encode(KEY, MESSAGE)
     assert result == EXPECTED_ENCODING
 
 
-def test_decode():
-    result = decode(KEY, EXPECTED_ENCODING)
+def test_decode(mocker):
+    app = Application()
+    result = app.decode(KEY, EXPECTED_ENCODING)
     assert result == MESSAGE
